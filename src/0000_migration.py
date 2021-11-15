a889281cba9b8931e15fee7840056bed7c9cdec8
Jared Magrath <magrathj@tcd.ie>
2021-11-15 20:34:30 +0000

    Update 0000_migration.py

--- src/0000_migration.py
+++ src/0000_migration.py
@@ -2,6 +2,20 @@ from delta_migrations import DeltaMigrationHelper
 
 print("Run Template example 0000_migration.py")
 
+
+def get_spark():
+    builder = (
+        pyspark.sql.SparkSession.builder
+        .master("local[1]")
+        .appName("delta_migrations")
+        .config("spark.sql.shuffle.partitions", "1")
+        .config("spark.default.parallelism", "1")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") 
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+    )
+    spark = spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    yield spark
+
 input = {
             'table_name': 'example1',
             'path': '/tmp/bronze/delta/example1', 
@@ -9,5 +23,7 @@ input = {
             'schema': StructType([StructField("script_name", StringType(), False), StructField("applied", TimestampType(), False)]),
             'partition_by': ["script_name"],
         }
+
+spark = get_spark()
 delta_migration_helper = DeltaMigrationHelper(spark)
 delta_migration_helper.modify_delta_table(input)
