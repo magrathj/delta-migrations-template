9d9db23c9a8eb3b155369cec293b26f15e0b5ead
Jared Magrath <magrathj@tcd.ie>
2021-11-15 20:32:00 +0000

    Update 0000_migration.py

--- src/0000_migration.py
+++ src/0000_migration.py
@@ -1,1 +1,13 @@
-print("hey from 000_migration.py")
+from delta_migrations import DeltaMigrationHelper
+
+print("Run Template example 0000_migration.py")
+
+input = {
+            'table_name': 'example1',
+            'path': '/tmp/bronze/delta/example1', 
+            'modify_type': 'create_table',
+            'schema': StructType([StructField("script_name", StringType(), False), StructField("applied", TimestampType(), False)]),
+            'partition_by': ["script_name"],
+        }
+delta_migration_helper = DeltaMigrationHelper(spark)
+delta_migration_helper.modify_delta_table(input)
